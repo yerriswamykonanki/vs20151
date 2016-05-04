43ff0f59bae85b6d8eea2eb7c52b1afccb24f75a
yerriswamykonanki <yerriswamy.konanki@ggktech.com>
2016-05-04 04:54:38 +0000

    firstcommit

---
+++ SampleDemo/SampleDemo/SampleDemo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using SampleDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        static List<tblStudent> _lstEmployee = new List<tblStudent>();
+        public ActionResult Index()
+        {
+
+            StudentsEntities db = new StudentsEntities();
+            return View(db.tblStudents.ToList());
+        }
+
+    }
+}
---
+++ SampleDemo/SampleDemo/SampleDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleDemo.MvcApplication" Language="C#" %>
